2572972a9fd40d8bef84835be65539f61fe24937
Dhruv Godambe <godambe@Dhruvs-MacBook-Air.local>
2020-06-28 13:43:08 +0530

    changed result formula

--- frontend/department_data.js
+++ frontend/department_data.js
@@ -10,33 +10,40 @@ fetch('/department_data', {
 		var str1 = ''
 		var result = 0;
 		for(var i = 0; i < 10; i++){
+			var avg = 0;
+			var overall = 0;
 			for(var j = 0; j < 4; j++){
-				var avg;
 				switch(j){
 					case 0:
 						var var1 = 'A' + (i+1).toString()
-						avg = faculty[var1]
+						avg = avg + faculty[var1]
+						overall = overall + faculty[var1]
 						str1 = str1.concat(`<td>${faculty[var1]}</td>`)
 						break;
 					case 1:
 						var var2 = 'B' + (i+1).toString()
 						avg = avg + faculty[var2]
+						overall = overall + faculty[var2]
 						str1 = str1.concat(`<td>${faculty[var2]}</td>`)
 						break;
 					case 2:
 						var var3 = 'C' + (i+1).toString()
+						overall = overall + faculty[var3]
 						str1 = str1.concat(`<td>${faculty[var3]}</td>`)
 						break;
 					case 3:
 						var var4 = 'D' + (i+1).toString()
+						overall = overall + faculty[var4]
 						str1 = str1.concat(`<td>${faculty[var4]}</td>`)
 						break;
 				}
-				result = result + avg
 			}
+			result = result + (avg/(faculty[var1] + faculty[var2] + faculty[var3] + faculty[var4]))
+			console.log('avg: ', avg)
+			console.log('result: ', result)
 		}
-		result = result/10
-		console.log('id: ', faculty.id, 'result: ', result)
+		result = Math.ceil(result*10)
+		console.log('overall: ', overall, 'result: ', result)
 		$('table').append(`
 			<tr>
 				<td>${faculty.id}</td>
